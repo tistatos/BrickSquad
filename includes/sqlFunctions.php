642592f76b415aa387acfdbb141d2f0fe74bcbad
Erik Larsson <tistatos@gmail.com>
2013-01-09 15:29:19 +0100

    forgot file

--- includes/sqlFunctions.php
+++ includes/sqlFunctions.php
@@ -18,13 +18,20 @@ function CreateResultTable($queryResult, $typeOfSearch)
 	{
 		echo('<table class="info">');
 		echo('<tr class="header">');
+
 		for($i=0; $i<mysql_num_fields($queryResult); $i++)
 		{
 			$fieldname = mysql_field_name($queryResult, $i);
 			echo("<th>$fieldname</th>");
 		}
-		echo "</tr>\n";
 
+		echo("<th>Images</th>");
+
+		echo "</tr>\n";
+		echo("</table>");
+		echo("<div class='outerSearchResult'>");
+		echo("<div id='searchResult' class='innerSearchResult'>");
+		echo('<table class="info">');
 		while($row = mysql_fetch_row($queryResult))
 		{
 			if($typeOfSearch == "partID" || $typeOfSearch == "partName")
@@ -36,13 +43,44 @@ function CreateResultTable($queryResult, $typeOfSearch)
 				$searchType = "setID";
 			}
 			echo('<tr class="parts" onclick="location.href=\'./search.php?searchType='.$searchType.'&amp;searchString='.$row[0].'&amp;searchYear=\'">');
+			if(strlen ($row[2]) > 40)
+			{
+				//shorten name
+				$row[2] = substr($row[2], 0, 40) . "...";
+			}
 			for($i=0; $i<mysql_num_fields($queryResult); $i++)
 			{
 				echo("<td>$row[$i]</td>");
 			}
+
+			if($searchType == "setID")
+			{
+				$imgLink = getPictureLink($row[0], "S");
+			}
+			if($searchType =="partID")
+			{
+				$colors = GetPartColors($row[0]);
+        		$color = 0;
+        		if($colors != "")
+        		{
+          			$color = $colors[0];
+          			$imgLink = getPictureLink($row[0], "P/$color");
+        		}
+			}
+			if($imgLink != "")
+			{
+				echo("<td><img height='80' src='". $imgLink . "' alt=$row[0] /></td>");
+			}
+			else
+			{
+				echo("<td><img height='80' src='./images/noimg.png' alt='' /></td>");
+			}
+
 			echo("</tr>\n");
 		}
 		echo("</table>\n");
+		echo("</div>");
+		echo("</div>");
 	}
 }
 
@@ -167,5 +205,41 @@ function validate_image($Part, $Color)
 	else
 	{
 		print("<td> .PNG fil </td>");
+	}
+}
+
+function CreatePaging($numberofResults)
+{
+	echo("<p>");
+	echo("Pages: ");
+	$NumberOfPages = ceil($numberofResults/11);
+	for($i =0; $i < $NumberOfPages; $i++)
+	{
+		echo("<a onclick='javascript:scrollResult($i);' href='#result'>".($i+1)." &nbsp;</a>");
+	}
+	echo("</p>");
+}
+
+function GetPartColors($partID)
+{
+	$queryString = 	"SELECT DISTINCT colors.ColorID, colors.Colorname FROM inventory
+							JOIN colors ON inventory.ColorID = colors.ColorID
+							WHERE inventory.ItemID = '$partID'";
+	if(mysql_ping())
+	{
+		$colorContents = mysql_query($queryString);
+		if(numOfResults($colorContents) != 0)
+		{
+			$colorArray = array();
+			while($row = mysql_fetch_row($colorContents))
+			{
+					$colorArray[] = $row[0];
+			}
+			return $colorArray;
+		}
+		else
+		{
+			return "";
+		}
 	}
 }
